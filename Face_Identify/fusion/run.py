73495ffb878c7f65e8cc5977a6bcd65c489c228d
linbird <zhangqw1213@gmail.com>
2020-05-03 13:02:28 +0800

    PC不跑

--- Face_Identify/fusion/run.py
+++ Face_Identify/fusion/run.py
@@ -61,44 +61,24 @@ while True:
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:,:,::-1]
 
-    # time1 = time.time()
+    time1 = time.time()
     # Find all the faces and face enqcodings in the frame of video
     face_locations = location.detect(rgb_frame, ort_session, input_name)
-    # time2 = time.time()
-    # print("face_image", rgb_frame.shape, frame.shape)
+    time2 = time.time()
 
-    # multi_encode = partial(face_recognition.face_encodings, face_image=rgb_frame)
     pool = Pool(cpu_count()-1)
     face_encodings = []
-    # time3 = time.time()
-    # print("face_locations: ", len(face_locations))
     for face_location in face_locations:
-        #     # face_encodings = face_recognition.face_encodings(rgb_frame, [face_location])
-
-    #     # print(pool.apply_async(face_recognition.face_encodings, (rgb_frame, face_location)).get())
         face_encoding = pool.apply_async(face_recognition.face_encodings, (rgb_frame, [face_location])).get()
         face_encodings.append(face_encoding[0])
-        # face_encodings.append(face_encoding.get())
-    # # print((face_encoding.get()))
-    # # face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-    # # face_encodings.append(pool.map(multi_encode, face_locations))
+    # face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
     pool.close()
     pool.join()
-# p_list = [Process(target=count, args=(1,1)) for _ in range(10)]
 
-    # p_list = [Process(target=face_recognition.face_encodings, args=(rgb_frame, [face_location])) for face_location in face_locations]
-    # start = time.time()
-    # for p in p_list:
-    #     p.start()
-    # for p in p_list:
-    #     p.join()
-    # print("Multiprocess cpu", time.time() - start)
-
-    # time4 = time.time()
-    # print(time2 - time1, time4 - time3)
-    # origin = face_recognition.face_encodings(rgb_frame, face_locations)
-    # print(type(face_encodings) == type(origin))
-    # face_encodings = face_encodings[0]
+    time4 = time.time()
+    print(time2 - time1, time4 - time2)
+ 
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
